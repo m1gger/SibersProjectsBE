96f71a7d4bde5f6fa33b8b2e0b5001d079445d54
bhZL5:He/3f@hNZ <superdanil747@gmail.com>
2025-07-12 04:07:37 +0600

    f

--- Application/Features/TaskContext/Query/GetAllTasksQuery.cs
+++ Application/Features/TaskContext/Query/GetAllTasksQuery.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Application.Common.AbstructClasses;
+using Application.Common.Dto;
+using Application.Features.TaskContext.Dto;
+using Domain.Entities;
+using Domain.Enums;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +12,15 @@ using System.Threading.Tasks;
 
 namespace Application.Features.TaskContext.Query
 {
-    public class GetAllTasksQuery
+    public class GetAllTasksQuery : PagedQuery, IRequest<PagedDto<TaskDto>>
     {
         public int? ProjectId { get; set; }
+        public string? Search { get; set; }
+        public int? UserId { get; set; }
+        public TaskStatusEnum? TaskStatusEnum { get; set; }
+        public int? Priority { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
     }
 }

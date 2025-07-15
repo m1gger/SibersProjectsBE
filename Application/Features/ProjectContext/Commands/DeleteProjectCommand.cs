05ea44d58a570f5efa389640e9963aafca238591
bhZL5:He/3f@hNZ <superdanil747@gmail.com>
2025-07-15 22:56:59 +0600

    делит прожект

---
+++ Application/Features/ProjectContext/Commands/DeleteProjectCommand.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Application.Interfaces;
+
+namespace Application.Features.ProjectContext.Commands
+{
+    public class DeleteProjectCommand : IRequest<Unit>
+    {
+        public int ProjectId { get; set;    }
+    }
+
+
+
+
+    public class DeleteProjectCommandHandler : IRequestHandler<DeleteProjectCommand, Unit> 
+    {
+        private readonly ISibersDbContext _dbContext;
+        public DeleteProjectCommandHandler(ISibersDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Unit> Handle(DeleteProjectCommand request, CancellationToken cancellationToken)
+        {
+            var project = await _dbContext.Projects
+                .FirstOrDefaultAsync(p => p.Id == request.ProjectId, cancellationToken);
+            if (project == null)
+            {
+                throw new Exception("Project not found");
+            }
+            _dbContext.Projects.Remove(project);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}

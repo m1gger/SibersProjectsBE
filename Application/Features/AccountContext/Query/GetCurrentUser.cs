6d3476036e88b29ab96d2d0e814367c6628a90d1
DanielZOV <superdanil747@gmail.com>
2025-07-16 08:15:56 +0600

    CurrentUser

---
+++ Application/Features/AccountContext/Query/GetCurrentUser.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using Application.Features.AccountContext.Dto;
+using Microsoft.EntityFrameworkCore;
+using Application.Interfaces;
+using Application.Common.Helpers;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Features.AccountContext.Query
+{
+    public class GetCurrentUser : IRequest<UserDto>
+    {
+    }
+
+    public class GetCurrentUserHandler : IRequestHandler<GetCurrentUser, UserDto>
+    {
+        private readonly ISibersDbContext _context;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly UserManager<User> _userManager;
+        public GetCurrentUserHandler(ISibersDbContext context, ICurrentUserService currentUserService,UserManager<User> userManager)
+        {
+            _context = context;
+            _currentUserService = currentUserService;
+            _userManager = userManager;
+        }
+        public async Task<UserDto> Handle(GetCurrentUser request, CancellationToken cancellationToken)
+        {
+
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == _currentUserService.UserId);
+                var roles =await _userManager.GetRolesAsync(user);
+            var role = roles.FirstOrDefault() ?? "No Role Assigned";
+            var userDto = new UserDto
+            {
+                Id = user.Id,
+                FullName = user.GetUserFullName(),
+                Email = user.Email,
+                UserName = user.UserName,
+                Role = role
+            };
+
+
+            return userDto;
+        }
+    }
+
+}
--- WebAPI/Controllers/AccountController.cs
+++ WebAPI/Controllers/AccountController.cs
@@ -81,14 +81,39 @@ namespace WebAPI.Controllers
             return Ok(res);
         }
 
+        /// <summary>
+        /// Deletes a user based on the specified command.
+        /// </summary>
+        /// <remarks>This action requires the caller to have the <see cref="UserRoleEnum.Director"/> role.
+        /// Ensure that the provided <paramref name="request"/> contains valid data.</remarks>
+        /// <param name="request">The command containing the details of the user to be deleted.  This must include the necessary identifiers
+        /// and any required parameters.</param>
+        /// <returns>An <see cref="IActionResult"/> indicating the result of the operation.  Typically returns a success response
+        /// if the user was deleted successfully.</returns>
         [HttpDelete]
         [AuthorizeRole(UserRoleEnum.Director)]
+       
         public async Task<IActionResult> DeleteUser([FromQuery] DeleteUserCommand request)
         {
             var res = await Mediator.Send(request);
             return Ok(res);
 
 
+        }
+        /// <summary>
+        /// Retrieves the currently authenticated user's information.
+        /// </summary>
+        /// <remarks>This method requires the caller to be authenticated and authorized. Ensure that the
+        /// appropriate  authentication and authorization mechanisms are in place before invoking this method.</remarks>
+        /// <param name="query">The query object containing parameters for retrieving the current user's data.</param>
+        /// <returns>An <see cref="ActionResult{T}"/> containing a <see cref="UserDto"/> object with the authenticated user's
+        /// details.</returns>
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<UserDto>> GetCurrentUser([FromQuery] GetCurrentUser query)
+        {
+            var res = await Mediator.Send(query);
+            return Ok(res);
         }
     }
 }

53b64096d048987fd029a45a7d7ef641eaa88513
bhZL5:He/3f@hNZ <superdanil747@gmail.com>
2025-07-17 20:21:54 +0600

    ываываывавы

--- Application/Services/FileService.cs
+++ Application/Services/FileService.cs
@@ -1,7 +1,9 @@
 ﻿using Application.Interfaces;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using System;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Application.Services
@@ -9,6 +11,7 @@ namespace Application.Services
     public class FileService : IFileService
     {
         private readonly IWebHostEnvironment _env;
+
         public FileService(IWebHostEnvironment env)
         {
             _env = env;
@@ -22,21 +25,33 @@ namespace Application.Services
                 wwwRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
             }
 
+            projectName = MakeSafeName(projectName);
+
+            var safeFileName = MakeSafeName(Path.GetFileNameWithoutExtension(fileName));
+            var extension = Path.GetExtension(fileName);
+            var finalFileName = $"{safeFileName}_{Guid.NewGuid()}{extension}";
+
             var projectFolder = Path.Combine(wwwRootPath, "documents", projectName);
 
             if (!Directory.Exists(projectFolder))
                 Directory.CreateDirectory(projectFolder);
 
-            var filePath = Path.Combine(projectFolder, fileName);
+            var filePath = Path.Combine(projectFolder, finalFileName);
 
             using (var stream = new FileStream(filePath, FileMode.Create))
             {
                 await file.CopyToAsync(stream);
             }
 
-            // Возвращаем путь для доступа через браузер
-            var relativePath = $"/documents/{projectName}/{fileName}";
+            var relativePath = $"/documents/{projectName}/{finalFileName}";
             return relativePath;
+        }
+
+        private string MakeSafeName(string name)
+        {
+            // Удаляем всё, кроме букв, цифр, тире и подчёркивания
+            var safe = Regex.Replace(name, @"[^a-zA-Z0-9_\-]", "_");
+            return safe;
         }
     }
 }
